7ea5b3fffd62de82d50399fa4d1cab3cb6f82dba
Muhamad Habibi Azmi <m.habibiazmi@gmail.com>
2023-03-04 17:07:18 +0700

    add authorize
    add authorize
    
    add auth middleware
    
    create seeder for owners & banks
    
    add confirm password

--- src/modules/owners/services/archive.service.ts
+++ src/modules/owners/services/archive.service.ts
@@ -1,15 +1,16 @@
 import { OwnerEntity } from "../entities/owner.entity.js";
 import { OwnerRepository } from "../repositories/owner.repository.js";
-import DatabaseConnection from "@src/database/connection.js";
+import DatabaseConnection, { DocumentInterface } from "@src/database/connection.js";
 
 export class ArchiveOwnerService {
   private db: DatabaseConnection;
   constructor(db: DatabaseConnection) {
     this.db = db;
   }
-  public async handle(id: string, session: unknown) {
+  public async handle(id: string, doc: DocumentInterface, session: unknown) {
     const ownerEntity = new OwnerEntity({
-      archivedAt: new Date()
+      archivedBy_id: doc.archivedBy_id,
+      archivedAt: new Date(),
     });
 
     const ownerRepository = new OwnerRepository(this.db);
